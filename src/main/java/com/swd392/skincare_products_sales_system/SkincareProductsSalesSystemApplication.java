f0d40ee44f311d4f55aa2980d80aea078b24cf96
dominhit215@gmail.com <dominhit215@gmail.com>
2025-01-09 02:30:56 +0700

    first commit

---
+++ src/main/java/com/swd392/skincare_products_sales_system/SkincareProductsSalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.swd392.skincare_products_sales_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkincareProductsSalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkincareProductsSalesSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swd392/skincare_products_sales_system/SkincareProductsSalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swd392.skincare_products_sales_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkincareProductsSalesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
