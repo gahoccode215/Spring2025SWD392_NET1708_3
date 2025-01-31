98b419e5101dc8a4fc211f38e9aef66641f3a4a4
dominhit215@gmail.com <dominhit215@gmail.com>
2025-01-31 15:46:58 +0700

    feat: create Permission

---
+++ src/main/java/com/swd392/skincare_products_sales_system/model/Permission.java
@@ -0,0 +1,24 @@
+package com.swd392.skincare_products_sales_system.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "tbl_permission")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Permission extends AbstractEntity<Integer>{
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "description")
+    String description;
+
+    
+}
