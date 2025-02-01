064e2055cca39000b39a9dcc510b8e15ef43817d
dominhit215@gmail.com <dominhit215@gmail.com>
2025-02-01 10:37:39 +0700

    feat: add AuthenticationResponse dto

--- src/main/java/com/swd392/skincare_products_sales_system/service/AuthenticationService.java
+++ src/main/java/com/swd392/skincare_products_sales_system/service/AuthenticationService.java
@@ -1,8 +1,12 @@
 package com.swd392.skincare_products_sales_system.service;
 
+import com.swd392.skincare_products_sales_system.dto.request.IntrospectRequest;
 import com.swd392.skincare_products_sales_system.dto.request.SignInRequest;
+import com.swd392.skincare_products_sales_system.dto.response.IntrospectResponse;
 import com.swd392.skincare_products_sales_system.dto.response.TokenResponse;
 
 public interface AuthenticationService {
-    Intro
+    IntrospectResponse introspect(IntrospectRequest request);
+
+    
 }
