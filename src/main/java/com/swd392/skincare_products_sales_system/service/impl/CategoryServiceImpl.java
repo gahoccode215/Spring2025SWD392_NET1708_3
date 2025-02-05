3e0895e6420a17784116ff8fd7ac69bf52cdaaa5
dominhit215@gmail.com <dominhit215@gmail.com>
2025-02-06 03:12:40 +0700

    feat: add CategoryController

---
+++ src/main/java/com/swd392/skincare_products_sales_system/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,16 @@
+package com.swd392.skincare_products_sales_system.service.impl;
+
+import com.swd392.skincare_products_sales_system.service.CategoryService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class CategoryServiceImpl implements CategoryService {
+
+}
